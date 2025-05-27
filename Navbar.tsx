ecf3c916a28d8a76832b97621e1fc3f3cd2327ba
hkdas5464 <hkdas5464@gmail.com>
2025-05-27 09:27:04 +0530

    first commit

---
+++ Navbar.tsx
@@ -0,0 +1,23 @@
+// components/Navbar.js
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
